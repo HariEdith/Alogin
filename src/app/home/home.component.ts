48b1004bf694fdae81caf17168c305d2f41c733f
HariEdith <elanhariharan@gmail.com>
2024-03-22 15:00:08 +0530

    template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,14 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  users: { name: string, email: string, password: string }[] = [];
+  users: { name: string, email: string, password: string, age: number, mobileNumber: string }[] = [];
 
-  onLogin(loginData: { email: string, password: string }): void {
-    const { email, password } = loginData;
-    console.log('resived logindata ', loginData);
-  }
-
-  onRegister(registerData: { name: string, email: string, password: string }): void {
+  onRegister(registerData: { name: string, email: string, password: string, age: number, mobileNumber: string }): void {
     this.users.push(registerData);
     console.log('User registered:', registerData);
   }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,13 +7,86 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  @Output() register: EventEmitter<{ name: string, email: string, password: string }> = new EventEmitter<{ name: string, email: string, password: string }>();
+  @Output() register: EventEmitter<{ name: string, email: string, password: string, age: number, mobileNumber: string }> = new EventEmitter<{ name: string, email: string, password: string, age: number, mobileNumber: string }>();
 
   onRegister(registerForm: NgForm): void {
     if (registerForm.valid) {
-      const { name, email, password } = registerForm.value;
-      this.register.emit({ name, email, password });
+      const { name, email, password, age, mobileNumber } = registerForm.value;
+
+      // All fields required
+      if (!name || !email || !age || !password || !mobileNumber) {
+        console.log('All fields are required.');
+      }
+
+      // A-z
+      if (!this.validateName(name)) {
+        console.log('Please enter a valid Name.');
+      }
+
+      // min
+      if (!this.validateAge(age)) {
+        console.log('Minimum age required is 18.');
+
+      }
+
+      // Email 
+      if (!this.validateEmail(email)) {
+        console.log('Please enter a valid Email.');
+      }
+
+      // Password 
+      if (!this.validatePassword(password)) {
+        console.log('Password must have minimum 8 characters and at least one special character.');
+      }
+
+      // 10 min max
+      if (!this.validateMobileNumber(mobileNumber)) {
+        console.log('Mobile number must contain exactly 10 digits.');
+
+      }
+
+      // Emitting registration details
+      this.register.emit({ name, email, password, age, mobileNumber });
       registerForm.reset();
+    } else {
+      console.log('All fields are required.');
     }
+  }
+
+  validateName(name: string): boolean {
+    
+    if (!/^[A-Za-z]+$/.test(name)) {
+      return false;
+    }
+    return true;
+  }
+
+  validateAge(age: number): boolean {
+    
+    if (age < 18) {
+      return false;
+    }
+    return true;
+  }
+
+  validateEmail(email: string): boolean {
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      return false;
+    }
+    return true;
+  }
+
+  validatePassword(password: string): boolean {
+    if (!/^(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/.test(password)) {
+      return false;
+    }
+    return true;
+  }
+
+  validateMobileNumber(mobileNumber: string): boolean {
+    if (!/^\d{10}$/.test(mobileNumber)) {
+      return false;
+    }
+    return true;
   }
 }
